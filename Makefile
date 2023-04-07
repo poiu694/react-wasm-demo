2eb24746629b8de8c4e215e6307de02727053d27
Lee <43488305+poiu694@users.noreply.github.com>
2023-04-07 14:30:55 +0900

    chore: React + C Scaffold

---
+++ Makefile
@@ -0,0 +1,28 @@
+MODULE = matrix
+
+SRCDIR = src/bin
+SRCS = $(SRCDIR)/$(MODULE).c
+JSEXT = mjs
+
+.PHONY: all
+all: public/$(MODULE).wasm
+
+public/$(MODULE).wasm: $(SRCDIR)/$(MODULE).$(JSEXT)
+
+$(SRCDIR)/$(MODULE).$(JSEXT): $(SRCS)
+	emcc --no-entry $(SRCS) -o $(SRCDIR)/$(MODULE).$(JSEXT)  \
+	  --pre-js $(SRCDIR)/locateFile.js  \
+	  -s ENVIRONMENT='web'  \
+	  -s EXPORT_NAME='createModule'  \
+	  -s USE_ES6_IMPORT_META=0  \
+	  -s EXPORTED_FUNCTIONS='["_add", "_matrixMultiply", "_malloc", "_free"]'  \
+	  -s EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]'  \
+		-s ASSERTIONS \
+	  -O3
+	mv $(SRCDIR)/$(MODULE).wasm public/$(MODULE).wasm
+
+.PHONY: clean
+clean:
+	rm public/$(MODULE).wasm $(SRCDIR)/$(MODULE).$(JSEXT)
+
+re: clean all
---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import createModule from './bin/matrix.mjs';
+
+function App() {
+  const [addFunction, setAddFunction] = React.useState(null);
+  const [matrixMultiplyFunction, setMatrixMultiplyFunction] = React.useState(null);
+
+  React.useEffect(() => {
+    createModule().then((Module) => {
+      console.log(Module);
+      setAddFunction(() => Module.cwrap('add', 'number', ['number', 'number']));
+      setMatrixMultiplyFunction(() => wrapMatrixMultiply(Module));
+    });
+  }, []);
+
+  if (!addFunction || !matrixMultiplyFunction) {
+    return 'Loading...';
+  }
+
+  return (
+    <div className="App">
+      2 + 3 = {addFunction(2, 3)}
+      <br />
+      <br />
+      [[1, 2], [3, 4]] @ [[5, 6], [7, 8]] ={' '}
+      {JSON.stringify(
+        matrixMultiplyFunction(
+          [
+            [1, 2],
+            [3, 4],
+          ],
+          [
+            [5, 6],
+            [7, 8],
+          ]
+        )
+      )}
+    </div>
+  );
+}
+
+function wrapMatrixMultiply(Module) {
+  return function (firstMatrix, secondMatrix) {
+    const length = firstMatrix.length;
+
+    const flatFirst = new Float32Array(firstMatrix.flat());
+    const flatSecond = new Float32Array(secondMatrix.flat());
+    const buffer1 = Module._malloc(flatFirst.length * flatFirst.BYTES_PER_ELEMENT);
+    const buffer2 = Module._malloc(flatSecond.length * flatSecond.BYTES_PER_ELEMENT);
+    Module.HEAPF32.set(flatFirst, buffer1 >> 2);
+    Module.HEAPF32.set(flatSecond, buffer2 >> 2);
+
+    const resultBuffer = Module._malloc(flatFirst.length * flatFirst.BYTES_PER_ELEMENT);
+    const resultPointer = Module.ccall(
+      'matrixMultiply',
+      'number',
+      ['number', 'number', 'number', 'number'],
+      [buffer1, buffer2, resultBuffer, length]
+    );
+
+    const resultFlatArray = [];
+    for (let i = 0; i < length ** 2; i++) {
+      resultFlatArray.push(Module.HEAPF32[resultPointer / Float32Array.BYTES_PER_ELEMENT + i]);
+    }
+
+    const result = [];
+    while (resultFlatArray.length) {
+      result.push(resultFlatArray.splice(0, length));
+    }
+    Module._free(buffer1);
+    Module._free(buffer2);
+    Module._free(resultBuffer);
+    return result;
+  };
+}
+
+export default App;
